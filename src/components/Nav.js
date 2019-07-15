b1accd21f9f96df4d34e1a8945f2dffb03eb28b7
sahumphreys59 <sarah.humphreys59@gmail.com>
2019-07-15 12:59:37 -0700

    added note component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,16 @@
 import React from 'react';
 
 class Nav extends React.Component {
- render() {
-   return (
-     <div>
-       Nav Component
-     </div>
-   );
- }
+ 	render() {
+   	return (
+     	<div className="nav-container">
+       	<div className="nav-logo">Note</div>
+       	<div className="nav-button">
+       		+ Notes
+       	</div>	
+     	</div>
+   	);
+ 	}
 }
 
 export default Nav;
